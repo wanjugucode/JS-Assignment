895d174a29e9941534e27b10b613129c23aead07
wanjugucode <karobiaanastasia.com>
2021-03-21 20:29:17 +0300

    first commit

---
+++ practise.js
@@ -0,0 +1,25 @@
+function getGrade(){
+    var marks=88;
+    if(marks>=80){
+        console.log("A");
+    
+}
+else if(marks>=70){
+    console.log("B");
+
+}
+else if(marks>=60) {
+    console.log("C");
+}
+else if(marks>=50){
+    console.log("D");
+}
+else if(marks<=49){
+    console.log("E");
+
+}
+else{
+    console.log("F");
+}
+}
+getGrade();
